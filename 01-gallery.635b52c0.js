e67d244e6e2109af935d507877ce432949a2b531
SergeyBelokopytnyy <82495436+SergeyBelokopytnyy@users.noreply.github.com>
2022-01-31 17:57:40 +0000

    Deploying to gh-pages from @ SergeyBelokopytnyy/goit-js-hw-08@9f2c9c35309c98cbac19fa27794290108f9c4fc7 🚀

--- 01-gallery.635b52c0.js
+++ 01-gallery.635b52c0.js
@@ -6,6 +6,6 @@ var t=arguments[3];function e(t){return(e="function"==typeof Symbol&&"symbol"==t
 },{}],"kz9M":[function(require,module,exports) {
 
 },{}],"TfQ5":[function(require,module,exports) {
-"use strict";var e=require("./gallery-items"),l=r(require("simplelightbox"));function r(e){return e&&e.__esModule?e:{default:e}}require("simplelightbox/dist/simple-lightbox.min.css"),console.log(e.galleryItems);const a=document.querySelector(".gallery");function i(e){return e.map(({description:e,original:l,preview:r})=>`<a class="gallery__item" href="${l}">\n  <img class="gallery__image" src="${r}" alt="${e}"/>\n</a>`).join("")}a.insertAdjacentHTML("beforeend",i(e.galleryItems));var t=new l.default(".gallery a",{captionDelay:250,captionsData:"alt"});
+"use strict";var e=require("./gallery-items"),l=r(require("simplelightbox"));function r(e){return e&&e.__esModule?e:{default:e}}require("simplelightbox/dist/simple-lightbox.min.css");const i=document.querySelector(".gallery");function a(e){return e.map(({description:e,original:l,preview:r})=>`<a class="gallery__item" href="${l}">\n  <img class="gallery__image" src="${r}" alt="${e}"/>\n</a>`).join("")}i.insertAdjacentHTML("beforeend",a(e.galleryItems));var t=new l.default(".gallery a",{captionDelay:250,captionsData:"alt"});
 },{"./gallery-items":"q5Rn","simplelightbox":"yx6k","simplelightbox/dist/simple-lightbox.min.css":"kz9M"}]},{},["TfQ5"], null)
-//# sourceMappingURL=/goit-js-hw-08/01-gallery.9d544c74.js.map
+//# sourceMappingURL=/goit-js-hw-08/01-gallery.635b52c0.js.map
